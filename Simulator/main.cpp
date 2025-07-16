8e9b9652ab5d11097bdd78371ce6bedd089d90e8
Daniel Baruch <danielb534@gmail.com>
2025-07-16 11:26:43 +0300

    main - plugins loaded onto main

--- Simulator/main.cpp
+++ Simulator/main.cpp
@@ -5,7 +5,7 @@
 #include <dlfcn.h>
 
 #include "AlgorithmRegistrar.h"
-#include "GameManagerRegistrar.h"  // once implemented
+#include "GameManagerRegistrar.h"
 
 void printUsage(const char* prog) {
     std::cerr << "Usage: " << prog << " <-comparative|--competition> \\\n"
@@ -15,14 +15,20 @@ void printUsage(const char* prog) {
               << "       [--threads N] [--verbose]\n";
 }
 
+// Helper to turn "/path/to/Foo.so" into "Foo"
+static std::string stripSo(const std::string& path) {
+    auto slash = path.find_last_of("/\\");
+    std::string file = (slash==std::string::npos ? path : path.substr(slash+1));
+    auto dot   = file.rfind(".so");
+    return (dot==std::string::npos ? file : file.substr(0, dot));
+}
+
 int main(int argc, char* argv[]) {
-    std::string mapFile;
-    std::string managerSo;
+    std::string mapFile, managerSo;
     std::vector<std::string> algoSos;
-    bool modeComparative = false;
-    bool modeCompetition  = false;
-    int  numThreads       = 1;
-    bool verbose          = false;
+    bool modeComparative = false, modeCompetition = false;
+    int  numThreads     = 1;
+    bool verbose        = false;
 
     static struct option longOpts[] = {
         { "comparative", no_argument,       nullptr, 'c' },
@@ -37,39 +43,23 @@ int main(int argc, char* argv[]) {
 
     int opt;
     while ((opt = getopt_long(argc, argv, "cpm:g:a:t:v", longOpts, nullptr)) != -1) {
-        std::cout << "parsed opt: " << static_cast<char>(opt) << std::endl;
         switch (opt) {
-            case 'c':
-                modeComparative = true;
-                break;
-            case 'p':
-                modeCompetition = true;
-                break;
-            case 'm':
-                mapFile = optarg;
-                break;
-            case 'g':
-                managerSo = optarg;
-                break;
+            case 'c': modeComparative = true;            break;
+            case 'p': modeCompetition  = true;            break;
+            case 'm': mapFile          = optarg;         break;
+            case 'g': managerSo        = optarg;         break;
             case 'a': {
-                // split comma-separated list into algoSos
                 std::string list(optarg);
                 size_t pos = 0;
                 while ((pos = list.find(',')) != std::string::npos) {
                     algoSos.push_back(list.substr(0, pos));
                     list.erase(0, pos + 1);
                 }
-                if (!list.empty()) {
-                    algoSos.push_back(list);
-                }
+                if (!list.empty()) algoSos.push_back(list);
                 break;
             }
-            case 't':
-                numThreads = std::stoi(optarg);
-                break;
-            case 'v':
-                verbose = true;
-                break;
+            case 't': numThreads = std::stoi(optarg);    break;
+            case 'v': verbose    = true;                 break;
             default:
                 printUsage(argv[0]);
                 return 1;
@@ -89,29 +79,64 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // Debug print
     if (verbose) {
         std::cerr << "DEBUG: Mode               = "
                   << (modeComparative ? "Comparative" : "Competition") << "\n"
                   << "DEBUG: Map file           = " << mapFile << "\n"
                   << "DEBUG: Manager SO path    = " << managerSo << "\n"
                   << "DEBUG: Algorithm SO paths = ";
-        for (const auto& so : algoSos) {
-            std::cerr << so << " ";
-        }
+        for (auto& s : algoSos) std::cerr << s << " ";
         std::cerr << "\nDEBUG: Threads            = " << numThreads << "\n";
     }
 
-    // Placeholder: dynamic load manager and algorithms
-    // TODO: dlopen(managerSo.c_str(), RTLD_NOW);
-    //       GameManagerRegistrar::get().createManagerEntry(...)
-    //       validateLastRegistration / removeLast on failure
-    //
-    // TODO: for each algoSo in algoSos:
-    //         AlgorithmRegistrar::get().createAlgorithmFactoryEntry(...)
-    //         dlopen(algoSo.c_str(), RTLD_NOW);
-    //         validateLastRegistration / removeLast on failure
+    // 1) Dynamic‐load the GameManager
+    {
+        auto& gmReg = GameManagerRegistrar::getGameManagerRegistrar();
+        std::string name = stripSo(managerSo);
+        gmReg.createGameManagerEntry(name);
 
-    std::cout << "[Simulator] CLI parsed successfully.\n";
+        if (verbose) std::cerr << "[Simulator] Loading GM plugin: " << managerSo << "\n";
+        void* gmHandle = dlopen(managerSo.c_str(), RTLD_NOW);
+        if (!gmHandle) {
+            std::cerr << "Error: dlopen GM failed: " << dlerror() << "\n";
+            return 1;
+        }
+
+        try {
+            gmReg.validateLastRegistration();
+        } catch (const std::exception& e) {
+            std::cerr << "Error: GM registration for '" << name << "' failed\n";
+            gmReg.removeLast();
+            dlclose(gmHandle);
+            return 1;
+        }
+    }
+
+    // 2) Dynamic‐load each Algorithm
+    {
+        auto& algoReg = AlgorithmRegistrar::getAlgorithmRegistrar();
+        for (auto& path : algoSos) {
+            std::string name = stripSo(path);
+            algoReg.createAlgorithmFactoryEntry(name);
+
+            if (verbose) std::cerr << "[Simulator] Loading Algo plugin: " << path << "\n";
+            void* handle = dlopen(path.c_str(), RTLD_NOW);
+            if (!handle) {
+                std::cerr << "Warning: dlopen Algo '" << name << "' failed: " << dlerror() << "\n";
+                algoReg.removeLast();
+                continue;
+            }
+
+            try {
+                algoReg.validateLastRegistration();
+            } catch (...) {
+                std::cerr << "Warning: Algo registration for '" << name << "' failed\n";
+                algoReg.removeLast();
+                dlclose(handle);
+            }
+        }
+    }
+
+    std::cout << "[Simulator] CLI parsed and plugins loaded successfully.\n";
     return 0;
 }
