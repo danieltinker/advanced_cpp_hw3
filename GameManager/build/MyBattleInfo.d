7ac4d995506031a75d5b2e822f2d674b6208efc6
Daniel Baruch <danielb534@gmail.com>
2025-07-23 15:09:15 +0300

    removed GM battleInfo,myBattleInfo.

--- GameManager/build/MyBattleInfo.d
+++
@@ -1,6 +0,0 @@
-build/MyBattleInfo.o: src/MyBattleInfo.cpp \
-  include/GameManager_315634022/MyBattleInfo.h ../common/BattleInfo.h
-
-include/GameManager_315634022/MyBattleInfo.h:
-
-../common/BattleInfo.h:
--- GameManager/src/MyBattleInfo.cpp
+++
@@ -1,1 +0,0 @@
-#include "MyBattleInfo.h"
